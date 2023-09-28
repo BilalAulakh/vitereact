e67ee50ee3f630eeabf1e58b5794ae8d248eb7cc
BilalAulakh <bb807562@gmail.com>
2023-09-28 23:20:10 +0500

    add animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,25 @@
-
 import './App.css'
-
+import { useState } from 'react';
 function App() {
-
-
+  let [counter,setCounter]=useState(1)
+// let counter=5;
+const Addvalue=()=>{
+setCounter(counter+1)
+}
+const Removevalue=()=>{
+  setCounter(counter-1)
+  }
   return (
-    <div>
-<div className='h-72 w-72 bg-orange-700 mx-auto flex justify-center items-center'>
-  <h1 className='text-[50px] font-semibold'>Bilal Aulakh jutt</h1>
+  <>
+  <div className='w-full h-[100vh]  text-center bg-[url("https://cdn.pixabay.com/photo/2023/09/05/12/29/boy-8235025_1280.jpg")] flex flex-col justify-center items-center'><h1 className='bg-amber-400 text-center w-72 mx-auto h-16 flex justify-center items-center animate-[wiggle_3s_ease-in-out_infinite]'>counter is :{counter}</h1>
+<button
+onClick={Addvalue}
+ className='w-48 h-9 bg-slate-600 text-white rounded-[5px] mt-6'>Add</button>
+<br />
+<button onClick={Removevalue} className='w-48 h-9 bg-slate-600 text-white rounded-[5px] mt-4 animate-[wiggle1_1s_ease-in-out_infinite]'>Decrease</button>
 </div>
-    </div>
+
+  </>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,34 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
-  },
+    extend: {
+      keyframes: {
+        wiggle: {
+          '0%, 100%': { transform: 'translateY(20px) ' },
+          '50%': { transform: 'translateY(-90px)' },
+        },
+        wiggle1: {
+          '0%, 100%': { transform: 'translate(-50px) ' },
+          '50%': { transform: 'translate(50px)' },
+        },
+        spin: {
+          
+        '0%, 100%':{transform: 'rotate(0deg)'} 
+          },
+          '50%':
+           {transform: 'rotate(360deg)'} 
+          }
+        }
+      },
+      animation: {
+        wiggle: 'wiggle 1s ease-in-out infinite',
+        wiggle1: 'wiggle 1s ease-in-out infinite',
+        animation: 'spin 1s linear infinite'
+
+       
+      },
+    
+  
   plugins: [],
 }
 
