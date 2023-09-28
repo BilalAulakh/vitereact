047431f5a9dfc7b307149ef89eacde649f13e905
BilalAulakh <bb807562@gmail.com>
2023-09-28 21:20:49 +0500

    djdj

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <div>
 <div className='h-72 w-72 bg-orange-700 mx-auto flex justify-center items-center'>
-  <h1 className='text-[50px] font-semibold'>Bilal Aulakh</h1>
+  <h1 className='text-[50px] font-semibold'>Bilal Aulakh jutt</h1>
 </div>
     </div>
   )
